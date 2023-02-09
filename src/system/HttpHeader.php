f4783888ab67c71e39d4049445544caa5976fb40
Thoralf Rickert-Wendt <trw@acoby.de>
2023-02-09 16:00:25 +0100

    feat: add missing http header

--- src/system/HttpHeader.php
+++ src/system/HttpHeader.php
@@ -8,4 +8,8 @@ interface HttpHeader {
   const X_FORWARDED_PROTO = "X-Forwarded-Proto";
   const X_FORWARDED_PORT = "X-Forwarded-Port";
   const WWW_AUTHENTICATE = "WWW-Authenticate";
+  const CONTENT_TYPE = "Content-Type";
+  
+  const MIMETYPE_HTML = "text/html; charset=utf-8";
+  const MIMETYPE_JSON = "application/json; charset=UTF-8";
 }

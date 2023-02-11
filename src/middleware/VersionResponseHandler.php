6a55024b7918999075cc5166fd7835d2413f38a8
Thoralf Rickert-Wendt <trw@acoby.de>
2023-02-11 09:32:39 +0100

    feat: add version response handler to middleware options

---
+++ src/middleware/VersionResponseHandler.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace acoby\middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use acoby\services\ConfigService;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use acoby\system\HttpHeader;
+use acoby\system\Utils;
+
+/**
+ * A small handler to add per default server version.
+ * 
+ * @author Thoralf Rickert-Wendt
+ */
+class VersionResponseHandler implements MiddlewareInterface {
+  public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface {
+    $serverVersion = ConfigService::getString("version");
+    $clientVersion = $request->getHeader(HttpHeader::X_CLIENT_VERSION);
+    if (count($clientVersion) > 0) {
+      foreach ($clientVersion as $version) {
+        // must be more specfic and ignore bugfix-releases
+        if ($version !== $serverVersion) {
+          Utils::logError("Client Version ".$clientVersion." does not fit Server Version ".$serverVersion);
+        }
+      }
+    } else {
+      Utils::logInfo("Client did not send version information via Header ".HttpHeader::X_CLIENT_VERSION);
+    }
+    $response = $handler->handle($request);
+    return $response->withAddedHeader(HttpHeader::X_SERVER_VERSION, );
+  }
+}
--- src/system/HttpHeader.php
+++ src/system/HttpHeader.php
@@ -12,6 +12,8 @@ interface HttpHeader {
   const X_RESULT_MORE     = "X-RESULT-more";
   const X_RESULT_OFFSET   = "X-RESULT-offset";
   const X_RESULT_LIMIT    = "X-RESULT-limit";
+  const X_SERVER_VERSION  = "X-SERVER-version";
+  const X_CLIENT_VERSION  = "X-CLIENT-version";
   const LOCATION          = "Location";
 
   const MIMETYPE_HTML     = "text/html; charset=utf-8";
